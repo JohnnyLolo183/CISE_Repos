f6fe1ccd6f1d6b9cb81ab79cfc63076de27624a0
JohnnyLolo183 <sionezinzan@gmail.com>
2024-08-19 18:39:03 +1200

    frontend created worksheet3

---
+++ Worksheet3/worksheet3_frontend/src/app/show-book/[id]/page.tsx
@@ -0,0 +1,9 @@
+'use client'
+
+import ShowBookDetails from "/Users/Johnny Lolo/Desktop/CISE_Repos/CISE_Repos/Worksheet3/worksheet3_frontend/src/app/components/ShowBookDetails";
+
+export default function ShowBook() {
+  return (
+    <ShowBookDetails />
+  )
+}
